9cdb4efe7cf5ad8f09282a1047a8c8afc8cccdc2
Kirill Parakhin <kirill.parakhin@altenar.com>
2023-02-05 19:14:40 +0300

    Add some other UserDAO methods

--- src/main/java/ru/vlsu/ispi/DAO/UserDAO.java
+++ src/main/java/ru/vlsu/ispi/DAO/UserDAO.java
@@ -4,9 +4,9 @@ import ru.vlsu.ispi.beans.User;
 import ru.vlsu.ispi.daoimpl.IUserDAO;
 import ru.vlsu.ispi.enums.RoleType;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class UserDAO implements IUserDAO {
@@ -48,26 +48,123 @@ public class UserDAO implements IUserDAO {
 
     @Override
     public void Update(User user, Connection conn) throws SQLException {
-
+        String query = "UPDATE Users " +
+                "SET RoleId=?, Email=?, NickName=?, Password=?, Gender=?, ContactNumber=?, RegisterDate=?, BirthdayDate=?, Rating=?, Resume=?, Balance=?, Bonus=?" +
+                " WHERE Id=?";
+        try (PreparedStatement statement = conn.prepareStatement(query)) {
+            statement.setLong(1, user.getRoleId());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, user.getNickName());
+            statement.setString(4, user.getPassword());
+            statement.setString(5, user.getGender());
+            statement.setString(6, user.getContactNumber());
+            statement.setDate(7, (java.sql.Date) user.getRegisterDate());
+            statement.setDate(8, (java.sql.Date) user.getBirthdayDate());
+            statement.setFloat(9, user.getRating());
+            statement.setString(10, user.getResume());
+            statement.setFloat(11, user.getBalance());
+            statement.setFloat(12, user.getBonus());
+            statement.setLong(13, user.getId());
+            int rowsUpdated = statement.executeUpdate();
+            if (rowsUpdated > 0)
+                System.out.println("A sportsman was updated successfully");
+        } catch (SQLException ex){
+            System.out.println("Query was not successfully executed");
+        }
     }
 
     @Override
     public void Delete(Long id, Connection conn) throws SQLException {
-
+        String query = "DELETE FROM Users WHERE Id=?";
+        try (PreparedStatement statement = conn.prepareStatement(query)){
+            statement.setLong(1, id);
+            int rowsDeleted = statement.executeUpdate();
+            if (rowsDeleted > 0)
+                System.out.println("A user was successfully deleted");
+        } catch(SQLException ex){
+            System.out.println("Query was not successfully executed");
+        }
     }
 
     @Override
     public User FindUser(Long id, Connection conn) throws SQLException {
-        return null;
+        String query = "SELECT * FROM Users WHERE Id = " + Long.toString(id);
+        User currUser = new User();
+        try (Statement stmt = conn.createStatement()){
+            ResultSet rs = stmt.executeQuery(query);
+            if (rs.next()){
+                currUser.setId(rs.getLong("Id"));
+                currUser.setRoleId(rs.getInt("RoleId"));
+                currUser.setEmail(rs.getString("Email"));
+                currUser.setNickName(rs.getString("NickName"));
+                currUser.setPassword(rs.getString("Password"));
+                currUser.setContactNumber(rs.getString("ContactNumber"));
+                currUser.setRegisterDate(rs.getDate("RegisterDate"));
+                currUser.setBirthdayDate(rs.getDate("BirthdayDate"));
+                currUser.setRating(rs.getFloat("Rating"));
+                currUser.setResume(rs.getString("Resume"));
+                currUser.setBalance(rs.getFloat("Balance"));
+                currUser.setBonus(rs.getFloat("Bonus"));
+            }
+        } catch (SQLException ex){
+            System.out.println("Query was not successfully executed");
+        }
+        return currUser;
     }
 
     @Override
     public List<User> GetUsersByRole(RoleType roleType, Connection conn) throws SQLException {
-        return null;
+        String query = "SELECT * FROM Users WHERE RoleId = " + Integer.toString(roleType.getValue());
+        _context.Users = new HashMap<Long, User>();
+        try (Statement stmt = conn.createStatement()){
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()){
+                User currUser = new User();
+                currUser.setId(rs.getLong("Id"));
+                currUser.setRoleId(rs.getInt("RoleId"));
+                currUser.setEmail(rs.getString("Email"));
+                currUser.setNickName(rs.getString("NickName"));
+                currUser.setPassword(rs.getString("Password"));
+                currUser.setContactNumber(rs.getString("ContactNumber"));
+                currUser.setRegisterDate(rs.getDate("RegisterDate"));
+                currUser.setBirthdayDate(rs.getDate("BirthdayDate"));
+                currUser.setRating(rs.getFloat("Rating"));
+                currUser.setResume(rs.getString("Resume"));
+                currUser.setBalance(rs.getFloat("Balance"));
+                currUser.setBonus(rs.getFloat("Bonus"));
+                _context.Users.put(currUser.getId(), currUser);
+            }
+        } catch (SQLException ex){
+            System.out.println("Query was not successfully executed");
+        }
+        return new ArrayList<>(_context.Users.values());
     }
 
     @Override
     public List<User> GetAllUsers(Connection conn) throws SQLException {
-        return null;
+        String query = "SELECT * FROM Users";
+        _context.Users = new HashMap<Long, User>();
+        try (Statement stmt = conn.createStatement()){
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()){
+                User currUser = new User();
+                currUser.setId(rs.getLong("Id"));
+                currUser.setRoleId(rs.getInt("RoleId"));
+                currUser.setEmail(rs.getString("Email"));
+                currUser.setNickName(rs.getString("NickName"));
+                currUser.setPassword(rs.getString("Password"));
+                currUser.setContactNumber(rs.getString("ContactNumber"));
+                currUser.setRegisterDate(rs.getDate("RegisterDate"));
+                currUser.setBirthdayDate(rs.getDate("BirthdayDate"));
+                currUser.setRating(rs.getFloat("Rating"));
+                currUser.setResume(rs.getString("Resume"));
+                currUser.setBalance(rs.getFloat("Balance"));
+                currUser.setBonus(rs.getFloat("Bonus"));
+                _context.Users.put(currUser.getId(), currUser);
+            }
+        } catch (SQLException ex){
+            System.out.println("Query was not successfully executed");
+        }
+        return new ArrayList<>(_context.Users.values());
     }
 }
